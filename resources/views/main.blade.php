22196c0d1504400bcf9352acf9e317296907dd6d
Dmitro Sokolov <d.sokolov@spheremall.net>
2019-05-14 11:14:57 +0300

    add csrf

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     <div class="row h-100 justify-content-center align-items-center">
         <form action="/check" class="col-12">
+            {{ csrf_field() }}
             <div class="form-group">
                 <input type="text" class="form-control" placeholder="Введите строку">
             </div>
