f7f5901441a6e2e0a69bc3df4399c5e94139bce1
Sokolov <d.sokolov@spheremall.net>
2019-05-14 04:01:51 +0300

    add check method

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Ddis\Meta\Validator;
+use Illuminate\Http\Request;
+
+/**
+ * Class IndexController
+ *
+ * @package App\Http\Controllers
+ */
+class IndexController extends Controller
+{
+    public function index()
+    {
+        echo 1;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function check(Request $request)
+    {
+        $string    = $request->get('string');
+        $validator = new Validator();
+
+        try {
+            $response['result'] = $validator->setString($string)->validate();
+        } catch (\Exception $exception) {
+            $code              = 400;
+            $response["error"] = $exception->getMessage();
+        }
+
+        $response['string'] = $string;
+
+        return response($response, $code ?? 200)
+            ->header('Content-Type', 'application/json');
+    }
+}
